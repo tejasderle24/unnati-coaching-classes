aa66eb40a5edee63aca8c4075672c71e66ebfe4d
tejasderle24 <tejasderle24@gmail.com>
2025-08-13 16:45:58 +0530

    Gateway : Student authontication

---
+++ backend/gateway/app.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import expressProxy from 'express-http-proxy';
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+app.use(express.json());
+
+app.use('/students', expressProxy(process.env.STUDENTS_SERVICE_URL));
+
+
+app.listen(PORT, () => {
+    console.log(`Gateway server running on port ${PORT}`);
+});
